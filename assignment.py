32ef75a368645e01731adeddeaef81c075367af8
Sam Luby <samwluby@gmail.com>
2018-04-30 18:35:47 +0100

    pretty much done

--- assignment.py
+++ assignment.py
@@ -9,16 +9,14 @@ import urllib
 from bs4 import BeautifulSoup as bs
 import os
 import pandas as pd
-import scipy
+import itertools
 import re
+import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 import nltk
-from scipy.cluster.hierarchy import ward, dendrogram
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import cross_val_score
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import *
@@ -238,6 +236,7 @@ model.fit(data_train, target_train)
 predicted = model.predict(data_test)
 svc_acc = accuracy_score(target_test, predicted)
 
+
 ####################################
 # Evaluating Classification Models #
 ####################################
@@ -250,21 +249,75 @@ print("Accuracy using SVM method: " + str(round(svc_acc*100, 2)) + "%")
 
 # Cross-Validation Evaluation
 model = KNeighborsClassifier(n_neighbors=3)
-scores = cross_val_score(model, data_train, target_train, cv=5, scoring ='accuracy')
-print("Evaluating kNN classifier with Cross-Validation yields avg score: " + str(scores.mean()))
+# test_scores = cross_val_score(model, term_matrix, target, cv=5, scoring='accuracy')
+# print("Evaluating kNN classifier with Cross-Validation yields avg score: " + str(round(test_scores.mean()*100, 2)))
 
-model = MultinomialNB()
-scores = cross_val_score(model, data_train, target_train, cv=5, scoring="accuracy")
-print("Evaluating Naive Bayes classifier with Cross-Validation yields avg score: " + str(scores.mean()))
 
-model = SVC()
-scores = cross_val_score(model, data_train, target_train, cv=5, scoring="accuracy")
-print("Evaluating SVC classifier with Cross-Validation yields avg score: " + str(scores.mean()))
+
+# model = KNeighborsClassifier(n_neighbors=3)
+# scores = cross_val_score(model, data_train, target_train, cv=5, scoring ='accuracy')
+# print("Evaluating kNN classifier with Cross-Validation yields avg score: " + str(round(scores.mean()*100, 2)))
+#
+# model = MultinomialNB()
+# scores = cross_val_score(model, data_train, target_train, cv=5, scoring="accuracy")
+# print("Evaluating Naive Bayes classifier with Cross-Validation yields avg score: " + str(round(scores.mean()*100, 2)))
+#
+# model = SVC()
+# scores = cross_val_score(model, data_train, target_train, cv=5, scoring="accuracy")
+# print("Evaluating SVC classifier with Cross-Validation yields avg score: " + str(round(scores.mean()*100, 2)))
 
 
 # Confusion Matrix Evaluation
 model = KNeighborsClassifier(n_neighbors=3)
 model.fit(data_train, target_train)
 predicted = model.predict(data_test)
-cm = confusion_matrix(target_test, predicted)
-print(cm)
+cm_knn = confusion_matrix(target_test, predicted)
+print(cm_knn)
+
+model = MultinomialNB()
+model.fit(data_train, target_train)
+predicted = model.predict(data_test)
+cm_nb = confusion_matrix(target_test, predicted)
+print(cm_nb)
+
+model = SVC()
+model.fit(data_train, target_train)
+predicted = model.predict(data_test)
+cm_svc = confusion_matrix(target_test, predicted)
+print(cm_svc)
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+
+
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True category')
+    plt.xlabel('Predicted predicted category')
+
+classnames = ['sport', 'technology', 'business']
+
+plt.figure()
+plot_confusion_matrix(cm_knn, classnames, normalize=True, title='Confusion Matrix1')
+plt.figure()
+plot_confusion_matrix(cm_nb, classnames, normalize=True, title='Confusion Matrix2')
+plt.figure()
+plot_confusion_matrix(cm_svc, classnames, normalize=True, title='Confusion Matrix3')
+plt.show()
